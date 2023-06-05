0854e989fea16f3749234f96f789af7c75668cdc
rmindra <114856632+rmindra@users.noreply.github.com>
2023-06-05 08:10:03 +0700

    Migration from cdn to npm
    Simplified index.html and using @layer

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      spacing: {
+        13: "3.25rem",
+      },
+      fontFamily: {
+        inter: ["Inter"],
+      },
+      colors: {
+        wpu: "#bada55",
+        kopi: "#c0ffee",
+      },
+    },
+  },
+  plugins: [],
+};
